5700a57a9d0e4f0d433242ad00b25e8ed658dd9a
Anton Sakovich <anton.sakovich@softteco.com>
2022-12-12 21:10:50 +0300

    Update `Program`
    Align the demo code to the recent changes.

--- MsdnTableParser/Program.fs
+++ MsdnTableParser/Program.fs
@@ -1,21 +1,32 @@
-﻿open MsdnTableParser.MsdnSectionParser
+﻿open MsdnTableParser.HtmlFetcher
+open MsdnTableParser.MarkdownParser
+open MsdnTableParser.SectionParser
 
-let table: Table =
-    Rows [
-        Values (["Option name"; "Name"; ""]);
-        Values (["Option value"; "value 1"]);
-        Values ([""; "value 2"]);
-        Values (["Default option value"; "default"])
-    ]
+let path = "/code-analysis/style-rules/csharp-formatting-options"
+let urlToFetchFrom = "https://raw.githubusercontent.com/dotnet/docs/main/docs/fundamentals" + path + ".md"
+let urlToPrepend = "https://learn.microsoft.com/en-us/dotnet/fundamentals" + path
 
-let section: Section =
-    {
-        Header =
-            {
-                Title = "Title";
-                Link = "Link";
-            };
-        Table = table;
-    }
+let text =
+    async {
+        use httpClient = new System.Net.Http.HttpClient()
+        let! text = fetchPageAsync httpClient "text/plain" urlToFetchFrom |> Async.AwaitTask
+        return text
+    } |> Async.RunSynchronously
 
-section |> parseSection |> printf "%A"
+let sections =
+    match text with
+    | Some value -> Some (parseMarkdown urlToPrepend value)
+    | None -> None
+
+let metadata =
+    match sections with
+    | Some values ->
+        values
+        |> Seq.map parseSection
+        |> Seq.choose id
+        |> Some
+    | None -> None
+
+printfn "%A" sections
+printfn "%A" metadata
+printfn "%s" "Hello World!"
