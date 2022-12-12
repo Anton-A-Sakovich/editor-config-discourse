3f0dcdc1130c941988a25fb2c69105f93233d664
Anton Sakovich <anton.sakovich@softteco.com>
2022-12-12 21:09:26 +0300

    Update `HtmlFetcher`
    Add the expected content type of the response as a function parameter.

--- MsdnTableParser/HtmlFetcher.fs
+++ MsdnTableParser/HtmlFetcher.fs
@@ -3,10 +3,10 @@ namespace MsdnTableParser
 module HtmlFetcher =
     open System.Net.Http
 
-    let fetchPageAsync (httpClient:HttpClient) (url:string) =
+    let fetchPageAsync (httpClient:HttpClient) (contentType:string) (url:string) =
         task {
             let! response = httpClient.GetAsync(url)
-            if response.Content.Headers.ContentType.MediaType = "text/html" then
+            if response.Content.Headers.ContentType.MediaType = contentType then
                 let! text = response.Content.ReadAsStringAsync()
                 return Some text
             else
