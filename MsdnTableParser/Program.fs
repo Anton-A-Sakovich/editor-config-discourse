6e8ef95dee28898bfb7e19360d5d82acb2895a80
Anton Sakovich <anton.sakovich@softteco.com>
2022-12-13 19:51:57 +0300

    Update `Program` to emit YAML

--- MsdnTableParser/Program.fs
+++ MsdnTableParser/Program.fs
@@ -1,32 +1,46 @@
 ﻿open MsdnTableParser.HtmlFetcher
 open MsdnTableParser.MarkdownParser
 open MsdnTableParser.SectionParser
+open YamlDotNet.Serialization
 
-let path = "/code-analysis/style-rules/csharp-formatting-options"
-let urlToFetchFrom = "https://raw.githubusercontent.com/dotnet/docs/main/docs/fundamentals" + path + ".md"
-let urlToPrepend = "https://learn.microsoft.com/en-us/dotnet/fundamentals" + path
+let githubUrlPrefix = "https://raw.githubusercontent.com/dotnet/docs/main/docs/fundamentals"
+let msdnUrlPrefix = "https://learn.microsoft.com/en-us/dotnet/fundamentals"
 
-let text =
-    async {
-        use httpClient = new System.Net.Http.HttpClient()
-        let! text = fetchPageAsync httpClient "text/plain" urlToFetchFrom |> Async.AwaitTask
-        return text
-    } |> Async.RunSynchronously
+[<EntryPoint>]
+let main args =
+    if args.Length = 0 then
+        printfn "No URL provided."
+        1
+    else
+        let url = args[0]
+        if not (url.StartsWith(githubUrlPrefix)) then
+            printfn "URL is incorrect."
+            2
+        else
+            let urlToPrepend = url.Replace(githubUrlPrefix, msdnUrlPrefix)
 
-let sections =
-    match text with
-    | Some value -> Some (parseMarkdown urlToPrepend value)
-    | None -> None
+            let text =
+                async {
+                    use httpClient = new System.Net.Http.HttpClient()
+                    let! text = fetchPageAsync httpClient "text/plain" url |> Async.AwaitTask
+                    return text
+                } |> Async.RunSynchronously
 
-let metadata =
-    match sections with
-    | Some values ->
-        values
-        |> Seq.map parseSection
-        |> Seq.choose id
-        |> Some
-    | None -> None
+            match text with
+            | None ->
+                printfn "Error fetching the markdown from GitHub."
+                3
+            | Some value ->
+                let optionsMetadata =
+                    parseMarkdown urlToPrepend value
+                    |> Seq.map parseSection
+                    |> Seq.choose id
 
-printfn "%A" sections
-printfn "%A" metadata
-printfn "%s" "Hello World!"
+                let serializer =
+                    SerializerBuilder()
+                     .DisableAliases()
+                     .Build()
+
+                serializer.Serialize(optionsMetadata) |> printfn "%s"
+
+                0
