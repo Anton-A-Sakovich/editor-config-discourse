d072046a1e72bdf996f393b761e0dea1936239a2
Anton Sakovich <anton.sakovich@softteco.com>
2022-12-17 00:10:28 +0300

    Update `EditorconfigParser`
    Add a special syntax to accommodate both a reference to an issue and
    an additional textual comment after the reference in a single line.

--- MarkdownUpdater/EditorconfigParser.fs
+++ MarkdownUpdater/EditorconfigParser.fs
@@ -7,12 +7,23 @@ module EditorconfigParser =
         Regex.Matches(contents, @"^\s*(\w+)\s*=\s*([\w:]+)\s*(?:#(.+))?$", RegexOptions.Multiline)
         |> Seq.fold (fun map regexMatch ->
             let ruleName = regexMatch.Groups[1].Value
-            let ruleValue = regexMatch.Groups[2].Value 
-            let issueId =
+            let ruleValue = regexMatch.Groups[2].Value
+
+            let comment' =
                 if regexMatch.Groups[3].Success then
                     Some regexMatch.Groups[3].Value
                 else
                     None
+
+            let issueId =
+                match comment' with
+                | Some comment ->
+                    let referenceMatch = Regex.Match(comment, @"^\(([^\)]+)\)")
+                    if referenceMatch.Success then
+                        Some referenceMatch.Groups[1].Value
+                    else
+                        None
+                | None -> None
 
             let rule =
                 {
