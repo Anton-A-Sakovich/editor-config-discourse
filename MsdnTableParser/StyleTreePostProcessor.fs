ae009a8969f555480687762b800f83d7085464fb
Anton Sakovich <anton.sakovich@softteco.com>
2022-12-28 14:08:16 +0300

    Update `StyleTreePostProcessor`
    Remove duplicate rules.

--- MsdnTableParser/StyleTreePostProcessor.fs
+++ MsdnTableParser/StyleTreePostProcessor.fs
@@ -8,7 +8,19 @@ module StyleTreePostProcessor =
         | NoRules
         | Ok of StyleTree<MsdnPage>
 
-    let rec refineList name (collected:list<StyleTree<MsdnPage>>) (remaining:list<MsdnPageResult>) =
+    let rec removeDuplicateRulesLoop (set:Set<string>) (collected:list<MsdnRule>) (remaining:list<MsdnRule>) =
+        match remaining with
+        | [] -> (collected |> List.rev, set)
+        | head::tail ->
+            if set |> Set.contains head.Name then
+                removeDuplicateRulesLoop set collected tail
+            else
+                removeDuplicateRulesLoop (set |> Set.add head.Name) (head::collected) tail
+
+    let removeDuplicateRules (rules, set) =
+       removeDuplicateRulesLoop set [] rules
+
+    let rec refineListLoop name (collected:list<StyleTree<MsdnPage>>) (remaining:list<MsdnPageResult>) =
         match remaining with
         | [] ->
             match collected with
@@ -17,21 +29,27 @@ module StyleTreePostProcessor =
         | head'::tail ->
             match head' with
             | FailedToParse -> FailedToParse
-            | NoRules -> refineList name collected tail
-            | Ok head -> refineList name (head::collected) tail
+            | NoRules -> refineListLoop name collected tail
+            | Ok head -> refineListLoop name (head::collected) tail
 
-    let rec refineTree (tree:StyleTree<option<MsdnPage>>) =
+    let refineList name results =
+        refineListLoop name [] results
+
+    let rec refineTree set (tree:StyleTree<option<MsdnPage>>) =
         match tree with
-        | Page (name, page') ->
-            match page' with
-            | None -> FailedToParse
-            | Some {Rules = []} -> NoRules
-            | Some page -> Ok(Page(name, page))
-        | Section (name, children') ->
-            let children = children' |> List.map refineTree
-            refineList name [] children
+        | Page (name, maybePage) ->
+            match maybePage with
+            | None -> (FailedToParse, set)
+            | Some page ->
+                let (newRules, newSet) = removeDuplicateRules (page.Rules, set)
+                match newRules with
+                | [] -> (NoRules, newSet)
+                | _ -> (Ok (Page (name, { page with Rules = newRules; })), newSet)
+        | Section (name, children) ->
+            let (results, newSet) = children |> List.mapFold refineTree set
+            (refineList name results, newSet)
 
     let refine tree =
-        match refineTree tree with
-        | Ok tree -> Some tree
+        match refineTree Set.empty tree with
+        | (Ok tree, _) -> Some tree
         | _ -> None
