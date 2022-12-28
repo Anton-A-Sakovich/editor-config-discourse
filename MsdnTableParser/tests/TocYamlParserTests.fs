74eabebcb57e87063fee2a1f6a63bc51db49b15f
Anton Sakovich <anton.sakovich@softteco.com>
2022-12-28 22:41:07 +0300

    Fix `TocYamlParserTests`

--- MsdnTableParser/tests/TocYamlParserTests.fs
+++ MsdnTableParser/tests/TocYamlParserTests.fs
@@ -1,40 +1,40 @@
-namespace MsdnTableParser.Tests
-
-open MsdnTableParser
-open MsdnTableParser.TocYamlParser
+namespace EditorconfigDiscourse.MsdnTableParser.Tests
+open EditorconfigDiscourse.MsdnTableParser
+open EditorconfigDiscourse.StyleTree
+open EditorconfigDiscourse.Yaml
 open NUnit.Framework
 open YamlDotNet.RepresentationModel
 
 [<TestFixture>]
 type TocYamlParserTests() =
-    let assertParseResult (result:ParseResult<StyleTree<TocPage>>) (node:YamlNode) =
-        Assert.That(tryParse node, Is.EqualTo result)
+    let assertParseResult (result:ParseResult<StyleTree<string>>) (node:YamlNode) =
+        Assert.That(TocYamlParser.tryParse node, Is.EqualTo result)
 
     [<Test>]
     member _.ParsesPageMappingNode() =
         YamlMappingNode("name", "page_name", "href", "page_path")
-        |> assertParseResult (Success(Page({ Name = "page_name"; Href = "page_path"})))
+        |> assertParseResult (Parsed(Page("page_name", "page_path")))
 
     [<Test>]
     member _.FailsForEmptyMappingNode() =
-        YamlMappingNode() |> assertParseResult Failure
+        YamlMappingNode() |> assertParseResult Failed
 
     [<Test>]
     member _.FailsForMappingNodeWithoutNameOnly() =
-       YamlMappingNode("name", "page_name") |> assertParseResult Failure
-
-    [<Test>]
-    member _.FailsForMappingNodeWithIncorrectName() =
-        YamlMappingNode("name", YamlSequenceNode()) |> assertParseResult Failure
+       YamlMappingNode("name", "page_name") |> assertParseResult Failed
 
     [<Test>]
     member _.FailsForMappingNodeWithHrefOnly() =
-        YamlMappingNode("href", "page_path") |> assertParseResult Failure
+        YamlMappingNode("href", "page_path") |> assertParseResult Failed
+
+    [<Test>]
+    member _.FailsForMappingNodeWithIncorrectName() =
+        YamlMappingNode("name", YamlSequenceNode(), "href", "page_path") |> assertParseResult Failed
 
     [<Test>]
     member _.FailsForMappingNodeWithIncorrectHref() =
         YamlMappingNode("name", "page_name", "href", YamlMappingNode())
-        |> assertParseResult Failure
+        |> assertParseResult Failed
 
     [<Test>]
     member _.ParsesSectionMappingNodeWithExplicitName() =
@@ -44,10 +44,10 @@ type TocYamlParserTests() =
                 yield YamlMappingNode("name", "page_name_1", "href", "page_path_1") :> YamlNode
                 yield YamlMappingNode("name", "page_name_2", "href", "page_path_2")
             }))
-        |> assertParseResult (Success(
+        |> assertParseResult (Parsed(
                 Section("section_name", [
-                    Page({Name = "page_name_1"; Href = "page_path_1"});
-                    Page({Name = "page_name_2"; Href = "page_path_2"})])))
+                    Page("page_name_1", "page_path_1");
+                    Page("page_name_2", "page_path_2")])))
 
     [<Test>]
     member _.ParsesSectionMappingNodeWithoutName() =
@@ -56,11 +56,11 @@ type TocYamlParserTests() =
                 yield YamlMappingNode("name", "page_name_1", "href", "page_path_1") :> YamlNode
                 yield YamlMappingNode("name", "page_name_2", "href", "page_path_2")
             }))
-        |> assertParseResult (Success(
+        |> assertParseResult (Parsed(
                 Section("Root", [
-                    Page({Name = "page_name_1"; Href = "page_path_1"});
-                    Page({Name = "page_name_2"; Href = "page_path_2"})])))
+                    Page("page_name_1", "page_path_1");
+                    Page("page_name_2", "page_path_2")])))
 
     [<Test>]
     member _.FailsForMappingNodeWithIncorrectItems() =
-        YamlMappingNode("items", YamlMappingNode()) |> assertParseResult Failure
+        YamlMappingNode("items", YamlMappingNode()) |> assertParseResult Failed
