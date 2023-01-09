dec324ed2fc4dfdfa1214cbff8ebc1cd3f375121
Anton Sakovich <anton.sakovich@softteco.com>
2023-01-09 21:57:49 +0300

    Update `LinesEnumerator`
    Introduce "before start" and "after end" states, which throw an invalid
    operation exception if `Current` is accessed while in these states.

--- Utilities/src/LinesEnumerator.fs
+++ Utilities/src/LinesEnumerator.fs
@@ -8,22 +8,33 @@ type LinesEnumerator =
     val mutable private lineStart : int
     val mutable private index : int
 
-    new(chars) = { chars = chars; lineStart = 0; index = 0 }
+    new(chars) = { chars = chars; lineStart = 0; index = -1 }
 
     member this.Current with get() =
+        if this.index = -1 || this.index = this.chars.Length + 1 then
+            raise (invalidOp "The enumerator has not been started or has been completed.")
+
         this.chars.Slice(this.lineStart, this.index - this.lineStart)
 
     member this.MoveNext() =
-        while this.index < this.chars.Length && (this.chars[this.index] = '\r' || this.chars[this.index] = '\n') do
-            this.index <- this.index + 1
-
-        if this.index = this.chars.Length then
+        if this.index >= this.chars.Length then
+            if this.index = this.chars.Length then
+                this.index <- this.index + 1
             false
         else
-            this.lineStart <- this.index
-            while this.index < this.chars.Length && (this.chars[this.index] <> '\r' && this.chars[this.index] <> '\n') do
+            if this.index = -1 then
                 this.index <- this.index + 1
-            true
+
+            while this.index < this.chars.Length && (this.chars[this.index] = '\r' || this.chars[this.index] = '\n') do
+                this.index <- this.index + 1
+
+            if this.index = this.chars.Length then
+                false
+            else
+                this.lineStart <- this.index
+                while this.index < this.chars.Length && (this.chars[this.index] <> '\r' && this.chars[this.index] <> '\n') do
+                    this.index <- this.index + 1
+                true
 
 [<NoComparison; NoEquality>]
 type EnumeratedLines(text:string) =
